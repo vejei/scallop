62abc37195ed438b3255403c0b11c863f59969e4
zeleven <chengsnem@163.com>
2018-08-25 17:38:01 +0800

    update

--- app/src/main/java/io/github/zeleven/scallop/di/component/ActivityComponent.java
+++ app/src/main/java/io/github/zeleven/scallop/di/component/ActivityComponent.java
@@ -3,12 +3,16 @@ package io.github.zeleven.scallop.di.component;
 import dagger.Component;
 import io.github.zeleven.scallop.di.module.ActivityModule;
 import io.github.zeleven.scallop.di.scope.ActivityScope;
+import io.github.zeleven.scallop.ui.modules.imageviewer.ImageViewerActivity;
 import io.github.zeleven.scallop.ui.modules.main.MainActivity;
-import io.github.zeleven.scallop.ui.modules.xiandu.categorydetail.XianDuListActivity;
+import io.github.zeleven.scallop.ui.modules.main.xiandu.list.XianDuListActivity;
+import io.github.zeleven.scallop.ui.modules.search.SearchActivity;
 
 @ActivityScope
 @Component(modules = ActivityModule.class, dependencies = ApplicationComponent.class)
 public interface ActivityComponent {
     void inject(MainActivity mainActivity);
     void inject(XianDuListActivity xianDuListActivity);
+    void inject(ImageViewerActivity imageViewerActivity);
+    void inject(SearchActivity searchActivity);
 }
