435b922321fae3c8af403fb42d4d50f74a0439eb
zeleven <chengsnem@163.com>
2018-08-25 17:38:32 +0800

    update

--- app/src/main/java/io/github/zeleven/scallop/di/component/FragmentComponent.java
+++ app/src/main/java/io/github/zeleven/scallop/di/component/FragmentComponent.java
@@ -3,12 +3,12 @@ package io.github.zeleven.scallop.di.component;
 import dagger.Component;
 import io.github.zeleven.scallop.di.module.FragmentModule;
 import io.github.zeleven.scallop.di.scope.FragmentScope;
-import io.github.zeleven.scallop.ui.modules.home.HomeFragment;
-import io.github.zeleven.scallop.ui.modules.home.tabpage.GanHuoPageFragment;
-import io.github.zeleven.scallop.ui.modules.more.MoreFragment;
-import io.github.zeleven.scallop.ui.modules.pictures.PicturesFragment;
-import io.github.zeleven.scallop.ui.modules.xiandu.XianDuFragment;
-import io.github.zeleven.scallop.ui.modules.xiandu.tabpage.XianDuPageFragment;
+import io.github.zeleven.scallop.ui.modules.main.home.HomeFragment;
+import io.github.zeleven.scallop.ui.modules.main.home.tabpage.GanHuoPageFragment;
+import io.github.zeleven.scallop.ui.modules.main.more.MoreFragment;
+import io.github.zeleven.scallop.ui.modules.main.pictures.PicturesFragment;
+import io.github.zeleven.scallop.ui.modules.main.xiandu.XianDuFragment;
+import io.github.zeleven.scallop.ui.modules.main.xiandu.tabpage.XianDuPageFragment;
 
 @FragmentScope
 @Component(modules = FragmentModule.class, dependencies = ApplicationComponent.class)
