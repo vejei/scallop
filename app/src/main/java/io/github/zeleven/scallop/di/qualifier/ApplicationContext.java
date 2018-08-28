e0d0c93662368a3ab703f9edf85d8e2f6221c161
zeleven <chengsnem@163.com>
2018-08-28 16:32:12 +0800

    dagger qualifier

---
+++ app/src/main/java/io/github/zeleven/scallop/di/qualifier/ApplicationContext.java
@@ -0,0 +1,11 @@
+package io.github.zeleven.scallop.di.qualifier;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ApplicationContext {
+}
