79b4d62004487047e4aae7bbd443ee3482bd158a
zeleven <chengsnem@163.com>
2018-08-28 22:15:01 +0800

    update

--- app/src/main/java/io/github/zeleven/scallop/utils/Constants.java
+++ app/src/main/java/io/github/zeleven/scallop/utils/Constants.java
@@ -1,5 +1,10 @@
 package io.github.zeleven.scallop.utils;
 
+import io.github.zeleven.scallop.BuildConfig;
+
 public class Constants {
     public static final String BASE_URL = "http://gank.io/api/";
+    public static final String ISSUE_PAGE_URL = "https://github.com/zeleven/scallop/issues";
+    public static final String PROJECT_PAGE_URL = "https://github.com/zeleven/scallop";
+    public static final String MY_EMAIL = BuildConfig.MY_EMAIL;
 }
