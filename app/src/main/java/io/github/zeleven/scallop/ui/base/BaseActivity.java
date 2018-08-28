9f4e8a573cc4a62e013da9748a2b323d944f8e25
zeleven <chengsnem@163.com>
2018-08-28 16:40:05 +0800

    update

--- app/src/main/java/io/github/zeleven/scallop/ui/base/BaseActivity.java
+++ app/src/main/java/io/github/zeleven/scallop/ui/base/BaseActivity.java
@@ -1,6 +1,11 @@
 package io.github.zeleven.scallop.ui.base;
 
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -9,12 +14,15 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.github.zeleven.scallop.R;
 import io.github.zeleven.scallop.Scallop;
 import io.github.zeleven.scallop.di.component.ActivityComponent;
 import io.github.zeleven.scallop.di.component.DaggerActivityComponent;
+import io.github.zeleven.scallop.ui.modules.main.MainActivity;
 import io.github.zeleven.scallop.utils.NetworkUtils;
 
 public abstract class BaseActivity extends AppCompatActivity implements BaseContract.View {
@@ -25,6 +33,10 @@ public abstract class BaseActivity extends AppCompatActivity implements BaseCont
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        if (this instanceof MainActivity) {
+            setTheme(R.style.AppTheme);
+        }
+        updateLanguage();
         super.onCreate(savedInstanceState);
         setContentView(getLayout());
         ButterKnife.bind(this);
@@ -65,5 +77,22 @@ public abstract class BaseActivity extends AppCompatActivity implements BaseCont
         if (appbarLayout != null) {
             appbarLayout.setElevation(enable ? getResources().getDimension(R.dimen.four_dp) : 0.0f);
         }
+    }
+
+    public void updateLanguage() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String languageCode = sharedPreferences.getString("app_language", "zh_CN");
+        Locale locale;
+        if (languageCode.contains("_")) {
+            String[] parts = languageCode.split("_");
+            locale = new Locale(parts[0], parts[1]);
+        } else {
+            locale = new Locale(languageCode);
+        }
+        Locale.setDefault(locale);
+        Resources resources = getResources();
+        Configuration configuration = resources.getConfiguration();
+        configuration.setLocale(locale);
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
     }
 }
