0f012793a9430fb82816295f393b2cb3d0cb4c9c
zeleven <chengsnem@163.com>
2018-08-28 16:33:34 +0800

    工具类

---
+++ app/src/main/java/io/github/zeleven/scallop/utils/NetworkUtils.java
@@ -0,0 +1,11 @@
+package io.github.zeleven.scallop.utils;
+
+import android.content.Context;
+
+public class NetworkUtils {
+    private NetworkUtils() {}
+
+    public static boolean isNetworkConnected(Context context) {
+        return false;
+    }
+}
